2d542d5f92f66fc9d68943738e0e4cb5a6221db0
Et3Aracno <luhatmaxime@gmail.com>
2025-02-17 16:15:40 +0100

    ok on a magouillerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr mais ça marche

---
+++ Project1/Objet.cpp
@@ -0,0 +1,14 @@
+#include "Objet.hpp"
+Objet::Objet(float x, float y) : Entity(x, y, sf::Color::Red) {}
+void Objet::update(float deltaTime, Grid& grid, sf::Vector2i& playerPos) {
+	
+}
+void Objet::coliP(Player& player, RectangleShape& forme, RenderWindow& window) {
+	if (player.shape.getGlobalBounds().intersects(forme.getGlobalBounds())) {
+		recupObj = true;
+		
+	}
+	if (recupObj == false){
+		window.draw(forme);
+	}
+}
